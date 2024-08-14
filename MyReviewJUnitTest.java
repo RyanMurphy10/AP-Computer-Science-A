26d72b650bde7d40c1f17f7ae5a3be473c43c504
Ryan Murphy <98363721+RyanMurphy10@users.noreply.github.com>
2024-08-13 21:31:57 -0400

    Add files via upload

---
+++ MyReviewJUnitTest.java
@@ -0,0 +1,30 @@
+import org.junit.*;
+/****************************************************
+ * JUnit Test for Review Class
+ *
+ * @author  Resendiz
+ * @version February 2021
+ ****************************************************/
+public class MyReviewJUnitTest {
+    /******************************************************
+     * Test default constructor - no input parameters
+     *****************************************************/
+    @Test
+    public void testConstructor() {
+        Review r = new Review(5, "Excellent Food");
+
+        Assert.assertEquals("Review rating should be ", 5, r.getRating());
+        Assert.assertEquals("Review comment should be ", "Excellent Food", r.getComment());
+    }
+
+    /******************************************************
+     * Test toString Method
+     *****************************************************/
+    @Test
+    public void testToStringMethod() {
+        Review r = new Review(5, "Excellent Food");
+
+        Assert.assertEquals("The toString method should be: ",
+                "Rating: 5 Comment: Excellent Food", r.toString());
+    }
+}
